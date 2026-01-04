f83c4df65551a1342c792ea8148b016d1ffcb8d0
Cao404 <vuhuucao789@gmail.com>
2026-01-04 17:57:13 +0700

    Add files via upload
    Signed-off-by: Cao404 <vuhuucao789@gmail.com>

---
+++ SellerHubDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SellerHub.Api.model;
+using SellerHub.Api.Models;
+
+namespace SellerHub.Api.Data;
+
+public class SellerHubDbContext : DbContext
+{
+    public SellerHubDbContext(DbContextOptions<SellerHubDbContext> options)
+         : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Models.SellerApplication> SellerApplications => Set<SellerApplication>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // USERS
+        modelBuilder.Entity<User>(e =>
+        {
+            e.HasIndex(x => x.Email).IsUnique();
+            e.Property(x => x.FullName).HasMaxLength(200).IsRequired();
+            e.Property(x => x.Email).HasMaxLength(255).IsRequired();
+            e.Property(x => x.Role).HasMaxLength(30).IsRequired();
+            e.Property(x => x.Status).HasMaxLength(30).IsRequired();
+        });
+
+        // SELLER APPLICATIONS
+        modelBuilder.Entity<SellerApplication>(e =>
+        {
+            e.HasIndex(x => x.UserId).IsUnique();
+            e.Property(x => x.Status).HasMaxLength(30).IsRequired();
+
+            e.HasOne(x => x.User)
+             .WithOne(u => u.SellerApplication)
+             .HasForeignKey<SellerApplication>(x => x.UserId)
+             .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
+}
---
+++ SellerHubDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace SellerHub.Api.Data;
+
+public class SellerHubDbContextFactory : IDesignTimeDbContextFactory<SellerHubDbContext>
+{
+    public SellerHubDbContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        var conn = config.GetConnectionString("Default");
+
+        var options = new DbContextOptionsBuilder<SellerHubDbContext>()
+            .UseSqlServer(conn)
+            .Options;
+
+        return new SellerHubDbContext(options);
+    }
+}
